5028650b825bde1ec7c946895c2d14d3b85728d1
Andreja Zivkovic <andrejazivkovic1@gmail.com>
2020-09-17 11:40:37 +0200

    Initial commit

---
+++ desktop/src/com/andrejazivkovic/flappybird/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.andrejazivkovic.flappybird.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.andrejazivkovic.flappybird.FlappyBird;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyBird(), config);
+	}
+}
